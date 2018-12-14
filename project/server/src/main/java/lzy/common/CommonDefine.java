0bfa393379fe417ccd8ea0cd8953458b4367dfdb
longzhiyou <long.zhiyou@bestskip.com>
2018-12-14 14:01:56 +0800

    添加权限相关

--- project/server/src/main/java/lzy/common/CommonDefine.java
+++ project/server/src/main/java/lzy/common/CommonDefine.java
@@ -13,24 +13,4 @@ public interface CommonDefine {
     int PASSWORD_ENCODER_STRENGTH=4;
 
 
-    /**
-     * 应用类别
-     */
-    String CLIENT_APP_TYPE_BUSINESS = "1"; //业务APP
-    String CLIENT_APP_TYPE_BACKGROUND = "0"; //后台管理APP
-
-    /**
-     *  床垫厂家类型
-     *  为了以后的扩展,后面的作为设备厂家类型提供
-     * [2017-10-13 add by longzhiyou]
-     */
-    String MATTRESS_MANUFACTURE_SOMNIC =  "somnic"; //杭州舒眠科技厂家
-    String MATTRESS_MANUFACTURE_DARMA =  "darma"; //深圳市大耳马科技有限公司
-    String MATTRESS_MANUFACTURE_HTJR =  "htjr"; //烟台汇通佳仁科技有限公司
-
-    String MANUFACTURE_CAT_CALL =  "catcall"; //多嘴猫呼叫
-
-
-
-
 }
